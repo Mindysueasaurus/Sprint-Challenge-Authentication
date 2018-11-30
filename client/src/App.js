1a60832f6abaf193aff4c473c1b2b741404c0760
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-30 10:56:15 -0600

    adds react app, finished index and app with routes

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  signout = () => {
+    localStorage.removeItem('jwt');
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <NavLink to="/">Sign Up</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/signin">Sign In</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/doggos">Doggos</NavLink>
+            &nbsp;|&nbsp;
+            <button onClick={this.signout}>Sign Out</button>
+          </nav>
+          <main>
+            <Route path="/" component={SignUp} exact />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/doggos" component={Doggos} />
+          </main>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
